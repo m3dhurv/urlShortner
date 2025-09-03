368bcde570c0644dfdd8a3317373d198d59cbf74
Dhruv <m3dhruv@gmail.com>
2025-09-03 14:33:33 +0530

    add vercel file

--- backend/index.js
+++ backend/index.js
@@ -66,4 +66,6 @@ app.get("/:shortUrl", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default app;
